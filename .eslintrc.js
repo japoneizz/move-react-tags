7bdce13761c593f78853464318e4ab04f28f42ec
Roberto Ono <japoneizz@gmail.com>
2022-12-23 18:42:46 +0000

    adding Jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,12 +25,12 @@ module.exports = {
     rules: {
         'no-unused-vars': 'off',
         '@typescript-eslint/no-unused-vars': ['error'],
-        '@typescript-eslint/no-var-requires': 'off',
+        // '@typescript-eslint/no-var-requires': 'off',
         'react-hooks/rules-of-hooks': 'error',
-        'react-hooks/exhaustive-deps': 'warn',
-        'react/prop-types': 'off',
-        'react/jsx-uses-react': 'off',
-        'react/react-in-jsx-scope': 'off',
-        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        'react-hooks/exhaustive-deps': 'error',
+        // 'react/prop-types': 'off',
+        // 'react/jsx-uses-react': 'off',
+        // 'react/react-in-jsx-scope': 'off',
+        // '@typescript-eslint/explicit-module-boundary-types': 'off',
     },
 };
---
+++ src/__tests__/App.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from '../App';
+
+describe('Test App root comp', () => {
+    it('shoud render App comp', () => {
+        render(<App />);
+        expect(screen.getByText(/Awesome/)).toBeInTheDocument();
+    });
+});
